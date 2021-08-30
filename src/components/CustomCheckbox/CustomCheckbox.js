5487f179a5d91d862866750c9fc719e403d90356
Inna Pikul <ipikul92@gmail.com>
2021-08-30 13:28:49 +0200

    add selected countries

--- src/components/CustomCheckbox/CustomCheckbox.js
+++ src/components/CustomCheckbox/CustomCheckbox.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import "./style.scss";
 
-const CustomCheckbox = ({onChange}) => {
-  const [checked, setChecked] = useState(false);
+const CustomCheckbox = ({ setSelected, selected, }) => {
+  const [checkedInitial, setCheckedInitial] = useState(false);
 
-  onChange = () => {
-    setChecked(!checked);
+  const handleClick = (e) => {
+    if (setSelected) {
+      setSelected(prevState => !prevState);
+    } else {
+        setCheckedInitial(prevState => !prevState);
+    }
   }
+  const checked = selected ? selected : checkedInitial;
 
   return (
     <div
       className={`checkbox ${checked ? "checked" : ""}`}
-      onClick={() => setChecked(!checked)}
+      onClick={(e) => handleClick(e)}
     ></div>
   );
 };
--- src/pages/CountryList.js
+++ src/pages/CountryList.js
@@ -1,31 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CustomCheckbox from "../components/CustomCheckbox/CustomCheckbox";
 import Navigation from "../components/Navigation/Navigation";
 import data from "../data/countries.json";
 import "./style.scss";
 
 const CountryList = () => {
+  const [initialData, setInitialData] = useState(data);
   const [country, setCountry] = useState("");
-  const [foundCountries, setFoundCountries] = useState(data);
-  const [itemChecked, setItemChecked] = useState(false);
+  const [foundCountries, setFoundCountries] = useState(initialData);
   const [checkedList, setCheckedList] = useState([]);
+  const [selected, setSelected] = useState(false);
+
+  // useEffect(() => {
+  //   console.log('sync')
+  //   setInitialData(initialData.map(item => ({...item, selected: false})));
+  // }, []);
 
   const handleFilterChange = (event) => {
     const keyWord = event.target.value;
     if (keyWord !== "") {
-      const result = data.filter((country) =>
+      const result = initialData.filter((country) =>
         country.name.toLowerCase().includes(keyWord.toLowerCase())
       );
       setFoundCountries(result);
     } else {
-      setFoundCountries(data);
+      setFoundCountries(initialData);
     }
     setCountry(keyWord);
   };
 
-  const handleCheckboxChange = () => {
+  const handleCheck = (position, countryCode) => {
     
+
+    setFoundCountries(
+      foundCountries.map((item) => {
+        if (countryCode === item.code && item.selected ) {
+          return {...item, selected: !item.selected}
+        }
+        if (countryCode === item.code ) {
+          return {...item, selected: !selected}
+        }
+        return item;
+      })
+    );
+
+    const selectedCountryArr = foundCountries.filter((item) => {
+      if(countryCode === item.code) {
+        return {...item};
+      }
+    });
+
+    setCheckedList((prevState) => {
+      const theSameItem  = prevState.find((item) => item.code === selectedCountryArr[0].code);
+      if (theSameItem) {
+        prevState.map((item, i, arr) => {
+          if(item.code === selectedCountryArr[0].code) {
+            arr.splice(i, 1);
+            //return prevState;
+            return arr;
+          }
+        })
+        return prevState;
+      }
+      return [...prevState, ...selectedCountryArr];
+    });
+    console.log('foundCountries', foundCountries);
+    console.log('checkedList', checkedList);
   }
+
+  console.log('foundCountries before r', foundCountries);
+  console.log('checkedList before r', checkedList);
 
   return (
     <div className="container">
@@ -36,23 +80,30 @@ const CountryList = () => {
         </div>
         <div className="row select-field">
           {foundCountries &&
-            (foundCountries.length > 0 ? (
+            (foundCountries?.length > 0 ? (
               <div className="col">
-                {foundCountries.map((item) => {
+                <p className="fw-bold">Countries:</p>
+                {foundCountries.map((item, i) => {
                   return (
                     <div key={item.code} className="d-flex mb-2">
-                      <CustomCheckbox onChange={handleCheckboxChange} />
-                      <div className="ml-2">{item.name}</div>
+                      <CustomCheckbox
+                        setSelected={() => handleCheck(i, item.code)}
+                        selected={foundCountries[i]?.selected}
+                      />
+                      <div className="ml-2">
+                        {item?.name}
+                        </div>
                     </div>
                   );
                 })}
               </div>
             ) : (
-              <div className="col">No countries found</div>
+              <p className="col">No countries found</p>
             ))}
           <div className="col">
-            {checkedList && (
-              <div className="col">
+            <p className="fw-bold">Selected Countries:</p>
+            {checkedList.length > 0 && (
+              <div>
                 {checkedList.map((item) => {
                   return (
                     <div key={item.code} className="mb-2">
