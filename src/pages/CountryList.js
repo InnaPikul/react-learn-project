493b0937c0140d58a60fb1215d1dbecb2869ec16
Inna Pikul <ipikul92@gmail.com>
2021-08-31 15:22:26 +0200

    change search filter

--- src/pages/CountryList.js
+++ src/pages/CountryList.js
@@ -12,17 +12,23 @@ const CountryList = () => {
 
   useEffect(() => {
     if (searchTerm !== "") {
-      const result = data.filter((country) =>
-        country.name.toLowerCase().includes(searchTerm.toLowerCase())
-      );
+      const result = foundCountries.map((country) => {
+        if(!country.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return { ...country, hidden: true }
+        }
+        return { ...country, hidden: false}
+      });
       setFoundCountries(result);
     } 
     else {
-      setFoundCountries(data); // need in case, when the keyWord word deleted
+      const data = foundCountries.map(country => {
+        return { ...country, hidden: false }
+      })
+      setFoundCountries(data); // need in case, when the keyWord word was deleted
     }
-  },[searchTerm]);
+  }, [searchTerm]);
 
-  const selectedFlag = (countryCode) => {
+  const selectCountry = (countryCode) => {
     return foundCountries.map((item) => {
       if (countryCode === item.code && item.selected ) {
         return {...item, selected: !item.selected}
@@ -56,10 +62,10 @@ const CountryList = () => {
                 <p className="fw-bold">Countries:</p>
                 {foundCountries.map((item, i) => {
                   return (
-                    <div key={item.code} className="d-flex mb-2">
+                    <div key={item.code} className={`list-item mb-2 ${foundCountries[i]?.hidden? 'hidden': ''}`}>
                       <CustomCheckbox
                         selected={foundCountries[i]?.selected}
-                        setSelected={() => setFoundCountries(selectedFlag(item.code))}
+                        setSelected={() => setFoundCountries(selectCountry(item.code))}
                       />
                       <div className="ml-2">
                         {item?.name}
